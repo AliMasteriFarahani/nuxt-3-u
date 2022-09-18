332435f92935b78e750fbe0f8f96bee80f32cd06
Ali-Farahani <alifarahani533@gmail.com>
2022-09-18 14:20:52 +0430

    landing page

--- app.vue
+++ app.vue
@@ -1,11 +1,18 @@
 <script setup lang="ts">
-
 </script>
 <template>
   <div>
+    <Nav />
+    <div class="container">
+      <h3>Welcome to our restaurant</h3>
+      <a href="/restaurant">got to restaurant</a>
+    </div>
   </div>
 </template>
 
 <style scoped>
-
+.container{
+  text-align: center;
+  margin-top: 5rem;
+}
 </style>
